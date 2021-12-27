09c0b25221a1e168b3eab57414e3247e3b5e010d
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-12-27 12:34:10 -0800

    api data placement

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState("");
+  const [reveal, setReveal] = useState(false);
 
   useEffect(() => {
     axios
       .get("https://api.coingecko.com/api/v3/coins/bitcoin")
       .then((res) => {
+        console.log(res.data);
         setData(res.data);
       })
       .catch((err) => {
@@ -16,14 +18,19 @@ function App() {
       });
   }, []);
 
-  // console.log(data.tickers);
+  const handleReveal = () => {
+    setReveal(!reveal)
+  }
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={data.image.small}/>
+        {/* <img src={data.image.small} alt="coin logo"/> */}
         <h1>{data.name}</h1>
-        {/* <a>{data.market_cap_rank}</a> */}
-        {/* <a>{data.tickers}</a> */}
+        <button onClick={handleReveal}>description</button>
+        {
+          reveal ? <p>{data.description.en}</p> : ""
+        }
       </header>
     </div>
   );
