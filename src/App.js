991dcf4d8c1ae41263d691c9e6700d96c2b2fb8a
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-12-28 22:56:18 -0800

    simple state change

--- src/App.js
+++ src/App.js
@@ -19,18 +19,22 @@ function App() {
   }, []);
 
   const handleReveal = () => {
-    setReveal(!reveal)
-  }
+    setReveal(true);
+  };
+  const handleHide = () => {
+    setReveal(false);
+  };
+
   console.log(data);
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={data.image.small} alt="coin logo"/> */}
-        <h1>{data.name}</h1>
-        <button onClick={handleReveal}>description</button>
-        {
-          reveal ? <p>{data.description.en}</p> : ""
-        }
+        <div className="coinDiv" onMouseEnter={handleReveal} onMouseLeave={handleHide}>
+          <h1>{data.name}</h1>
+        </div>
+        {/* <button onMouseEnter={handleReveal}>description</button> */}
+        {reveal ? <p>{data.description.en}</p> : ""}
       </header>
     </div>
   );
